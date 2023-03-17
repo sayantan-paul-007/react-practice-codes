a8c596854cb485468277bd28816c5a978a60c670
Sayantan Paul <sayantan@educba.com>
2023-03-17 16:35:09 +0530

    123

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
      <Practice4 num='121'>
       This is a paragraph of the Practice test files
      </Practice4>
-     <Practice5 num= '955' />
+     <Practice5 num= '9555' />
      <Practice6 />
     </div>
   );
